20fcd865ca50fb9878b70887caff762339590995
AlexKanj <alexkanj567@gmail.com>
2026-05-19 13:35:26 -0400

    Remove cloud block

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  cloud {
-    organization = "policy-as-code-training"
-    workspaces {
-      name = "tf-vault-qa-ak"
-    }
-  }
+  # cloud {
+  #   organization = "policy-as-code-training"
+  #   workspaces {
+  #     name = "tf-vault-qa-ak"
+  #   }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
